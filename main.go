92c2d58b2715ce044a9032e9b33b2ef4c564ad22
ritz <80481433+alterthefuture@users.noreply.github.com>
2021-08-24 14:08:49 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const token string = "ODc5NTI3MTQ1ODAwNjMwMzAz.YSRBfA.uk6UQRv-NL2NAWrU-g1v54lMY3U"
+
+var BotID string
+
+func main() {
+	dg, err := discordgo.New("Bot " + token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	u, err := dg.User("@me")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	BotID = u.ID
+
+	dg.AddHandler(messageHandler)
+
+	err = dg.Open()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Bot is running!")
+
+	<-make(chan struct{})
+	return
+
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == BotID {
+		return
+	}
+
+	if m.Content == "!ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "pong!")
+	}
+
+	if m.Content == "!hello" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Hello, "+m.Author.Username)
+	}
+
+	if m.Content == "!userinfo" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "User: "+m.Author.Username+"\nID: "+m.Author.ID+"\nTag: "+m.Author.Discriminator)
+	}
+}
